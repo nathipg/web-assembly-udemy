7cf8f80b24ac4dbe1ff8c2bd38e1ee95d63d9741
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-10-14 19:44:39 -0300

    Exporting C functions to Javascript

--- lib/demo.c
+++ lib/demo.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
   printf("WASM Ready - Testing with Emscripten HTML\n");
   return 1;
+}
+
+int getNum() {
+  return 22;
+}
+
+int getDoubleNum(int n) {
+  return n * 2;
+}
+
+char greetings[50];
+char* greet(char* name) {
+  if(strlen(name) > 40) {
+    return "Name too long";
+  }
+  strcpy(greetings, "Hello ");
+  return strcat(greetings, name);
 }
